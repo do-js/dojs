45a27e43ec9d67dc83e66f9a37fc3e094e236767
liuyin2004 <liuyin2004@gmail.com>
2016-11-06 09:07:51 +0800

    2个小问题

--- source/samples/script/dojs/core/print.js
+++ source/samples/script/dojs/core/print.js
@@ -1,7 +1,23 @@
 var dojs = require("dojs");
+var d1 = require("deviceone");
 
 module.exports.call = function() {
-	var data={name:"张晓丹", age:32}
-	dojs.core.p(data);
+	var data1 = {
+		name : "张晓丹",
+		age : 32
+	}
+	dojs.core.p(data1);
+	
+	var data2 = [ "张晓丹", "刘成伟" ];
+	dojs.core.p(data2);
+	
+	var hashdata = d1.mm("do_HashData");
+	hashdata.addData(data1)
+	dojs.core.p(hashdata);
+	
+	var listdata = d1.mm("do_ListData");
+	listdata.addData(data2);
+	dojs.core.p(listdata);
+	
 	dojs.core.toast("已在logger中打印");
 };
