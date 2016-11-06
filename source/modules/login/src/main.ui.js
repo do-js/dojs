1f6600c8439b279f05d3ebdfa807a4304aa06774
duanzj <duanzuojun@139.com>
2016-11-06 23:36:19 +0800

    完善登录模块

--- source/modules/login/src/main.ui.js
+++ source/modules/login/src/main.ui.js
@@ -5,13 +5,18 @@ dojs.style.css(ui("do_ALayout_back"), "dynamicButton");
 dojs.style.css(ui("do_Button_ok"), "dynamicButton");
 dojs.style.css(ui("do_ALayout_user_close"), "dynamicButton");
 dojs.style.css(ui("do_ALayout_password_close"), "dynamicButton");
+dojs.style.css(ui("do_ALayout_qq"), "dynamicButton");
+dojs.style.css(ui("do_ALayout_weixin"), "dynamicButton");
+dojs.style.css(ui("do_ALayout_sina"), "dynamicButton");
+dojs.style.css(ui("do_ALayout_reg"), "dynamicButton");
+dojs.style.css(ui("do_ALayout_fetchPwd"), "dynamicButton");
 
 dojs.page.allowHideKeyboard();
 
 var currentOption=null;
 
 dojs.page.onTouch(ui("do_Button_ok"), function() {
-	if (dojs.core.isNullData(currentOption.onConfirm)) return;
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
 	var _pwd=ui("do_TextField_password").text;
 	if (currentOption.currentOption=="md5"){
 		var md5=require("tools/crypt/md5");
@@ -25,8 +30,40 @@ dojs.page.onTouch(ui("do_Button_ok"), function() {
 		var sha256=require("tools/crypt/sha256");
 		_pwd=sha256.hex_sha256(_pwd).toLowerCase();
 	}
-	var _jsFile=require(currentOption.onConfirm);
-	_jsFile.invoke(ui("do_TextField_user").text, _pwd);
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"login_normal",
+		user:ui("do_TextField_user").text,
+		password:_pwd});
+});
+
+dojs.page.onTouch(ui("do_ALayout_reg"), function() {
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"register"});
+});
+
+dojs.page.onTouch(ui("do_ALayout_fetchPwd"), function() {
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"fetchPassword"});
+});
+
+dojs.page.onTouch(ui("do_ALayout_sina"), function() {
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"login_sina"});
+});
+
+dojs.page.onTouch(ui("do_ALayout_weixin"), function() {
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"login_weixin"});
+});
+
+dojs.page.onTouch(ui("do_ALayout_qq"), function() {
+	if (dojs.core.isNullData(currentOption.onCallback)) return;
+	var _jsFile=require(currentOption.onCallback);
+	_jsFile.invoke({type:"login_qq"});
 });
 
 ui("do_TextField_user").on("enter", function() {
@@ -54,10 +91,8 @@ function checkTextChange(){
 	if (ui("do_ALayout_user_close").visible &&
 			ui("do_ALayout_password_close").visible) {
 		ui("do_Button_ok").fontColor="FFFFFFFF";
-		ui("do_Button_ok").enabled=true;
 	} else {
-		ui("do_Button_ok").fontColor="ACACACFF";
-		ui("do_Button_ok").enabled=false;
+		ui("do_Button_ok").fontColor="CCCCCCFF";
 	}
 }
 ui("do_TextField_user").on("textChanged", function() {
@@ -81,13 +116,6 @@ if (!dojs.core.isNullData(data)) {
 	if (!dojs.core.isNullData(data.title)) {
 		ui("do_Label_title").text = data.title;
 	}
-	if (!dojs.core.isNullData(data.logo)) {
-		ui("do_ImageView_logo").source = data.logo;
-		ui("do_ImageView_logo").visible=true;
-	}
-	else{
-		ui("do_ImageView_logo").visible=false;
-	}
 	if (data.allowClose){
 		ui("do_ALayout_back").visible=true;
 		dojs.page.allowClose(ui("do_ALayout_back"));
@@ -95,6 +123,26 @@ if (!dojs.core.isNullData(data)) {
 	else{
 		ui("do_ALayout_back").visible=false;
 		dojs.page.allowExit();
+	}
+	if (!data.allowRegister) {
+		ui("do_ALayout_reg").visible = false;
+	}
+	if (!data.allowFetchPwd) {
+		ui("do_ALayout_fetchPwd").visible = false;
+	}
+	if (!data.allowSinaLogin && !data.allowWeiXinLogin && !data.allowQQLogin) {
+		ui("do_ALayout_thirdPart").visible = false;
+	}
+	else{
+		if (!data.allowSinaLogin) {
+			ui("do_ALayout_sina").visible = false;
+		}
+		if (!data.allowWeiXinLogin) {
+			ui("do_ALayout_weixin").visible = false;
+		}
+		if (!data.allowQQLogin) {
+			ui("do_ALayout_qq").visible = false;
+		}
 	}
 	if (!dojs.core.isNull(data.user) && typeof(data.user)=="object"){
 		if (!dojs.core.isNullData(data.user.hint)) {
