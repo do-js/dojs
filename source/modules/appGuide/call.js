73206f1d83765389bfaaec842e3e89ab0cb59eaa
duanzj <duanzuojun@139.com>
2016-11-13 14:56:13 +0800

    去掉对IOS7的考虑

--- source/modules/appGuide/call.js
+++ source/modules/appGuide/call.js
@@ -11,24 +11,12 @@ var dojs = require("dojs");
 var module_appGuide_callbackFile=null;
 module.exports.invoke = function(_option) {
 	module_appGuide_callbackFile=_option.onCallback;
-	if (!dojs.core.inPage()){
-		//从app.js进入，需要先打开一个底层基础页，
-		//防止关闭导览页时直接退出应用(IOS低版本容易出现这个问题)
-		dojs.core.openPage({
-			source : "source://modules/appGuide/src/base.ui",
-			animationType : "fade",
-			data : _option,
-			statusBarState : "transparent"
-		});
-	}
-	else{
-		dojs.core.openPage({
-			source : "source://modules/appGuide/src/main.ui",
-			animationType : "fade",
-			data : _option,
-			statusBarState : "transparent"
-		});
-	}	
+	dojs.core.openPage({
+		source : "source://modules/appGuide/src/main.ui",
+		animationType : "fade",
+		data : _option,
+		statusBarState : "transparent"
+	});
 };
 if (dojs.core.inPage()){
 	d1.sm("do_Page").on("result", function(data){
