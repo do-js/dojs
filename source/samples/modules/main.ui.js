e21f3e5864659c15ac3745b8ce94029a44ca436f
duanzj <duanzuojun@139.com>
2016-11-13 11:59:00 +0800

    调整模块的展示顺序

--- source/samples/modules/main.ui.js
+++ source/samples/modules/main.ui.js
@@ -3,18 +3,18 @@ var dojs=require("dojs");
 dojs.style.css(ui("do_ALayout_topbar"), "pageTopbar");
 
 var jsonData=[
-              {id:"inputTextField", Name:"文本输入页"},
-              {id:"scanBarcode", Name:"二维码扫描页"},
-              {id:"popupMenu", Name:"弹出式菜单"},
-              {id:"singleChoiceList", Name:"单选列表"},
-              {id:"mainFrame", Name:"主框架页"},
-              {id:"waittingLayer", Name:"遮盖层"},
-              {id:"simpleIndex", Name:"简单索引页"},
+              {id:"appGuide", Name:"应用引导页"},
               {id:"login", Name:"登录页"},
               {id:"register", Name:"注册页"},
               {id:"forgetPassword", Name:"重置密码"},
               {id:"realNameAuth", Name:"实名认证页"},
-              {id:"appGuide", Name:"应用引导页"}
+              {id:"mainFrame", Name:"主框架页"},
+              {id:"simpleIndex", Name:"简单索引页"},
+              {id:"popupMenu", Name:"弹出式菜单"},
+              {id:"inputTextField", Name:"文本输入页"},
+              {id:"scanBarcode", Name:"二维码扫描页"},
+              {id:"singleChoiceList", Name:"单选列表"},
+              {id:"waittingLayer", Name:"遮盖层"}
 ];
 
 var do_ListData=mm("do_ListData");
