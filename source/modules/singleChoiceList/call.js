da2a83a68dd133c045ae13dc19a745f19c9f1e87
duanzj <duanzuojun@139.com>
2016-11-13 10:14:47 +0800

    对于部分只能在ui里调用检查的模块，检查调用场景

--- source/modules/singleChoiceList/call.js
+++ source/modules/singleChoiceList/call.js
@@ -18,11 +18,16 @@ module.exports.invoke = function(_option, _callback){
 	});	
 };
 
-d1.sm("do_Page").on("result", function(data){
-	if (dojs.core.isNull(data) ||
-			dojs.core.isNull(data.moduleType) ||
-			data.moduleType != "$$singleChoiceList$$" ||
-			!module_singleChoiceList_callback) return;
-	module_singleChoiceList_callback.call(this, data.result);
-});
+if (!dojs.core.inPage()){
+	dojs.core.error("不允许在app.js中调用 singleChoiceList 模块");
+}
+else{
+	d1.sm("do_Page").on("result", function(data){
+		if (dojs.core.isNull(data) ||
+				dojs.core.isNull(data.moduleType) ||
+				data.moduleType != "$$singleChoiceList$$" ||
+				!module_singleChoiceList_callback) return;
+		module_singleChoiceList_callback.call(this, data.result);
+	});
+}
 
