8d9739cb0ec5530ed01b982d3933e2dbadcb29d8
duanzuojun <duanzuojun@139.com>
2016-11-23 12:51:17 +0800

    upadte 精简代码

--- source/samples/modules/main_1.ui.js
+++ source/samples/modules/main_1.ui.js
@@ -1,9 +1,3 @@
-var core=require("do/core");
-var http=require("do/http");
-var global=require("do/global");
-var page=require("do/page");
-var style=require("do/style");
-
 ui("$").setMapping({
 	"do_Label_name.text":"Name",
 	"do_Label_id.text":"id"
